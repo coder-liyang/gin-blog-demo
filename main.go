c4749fdb4f66b96ac4e873cd17c97b095b7ddc60
liyang <liyangcoder@163.com>
2020-11-23 17:15:39 +0800

    创建dockerfile

--- main.go
+++ main.go
@@ -4,33 +4,31 @@ import (
 	"fmt"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
-	"github.com/fvbock/endless"
-	"log"
-	"syscall"
+	"net/http"
 )
 
 func main() {
-	endless.DefaultReadTimeOut = setting.ReadTimeout
-	endless.DefaultWriteTimeOut = setting.WriteTimeout
-	endless.DefaultMaxHeaderBytes = 1 << 20
-	endPort := fmt.Sprintf(":%d", setting.HTTPPort)
-
-	server := endless.NewServer(endPort, routers.InitRouter())
-	server.BeforeBegin = func(add string) {
-		log.Printf("Actual pid is %d", syscall.Getpid())
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Server err: %v", err)
-	}
-
-	//router := routers.InitRouter()
-	//s := &http.Server{
-	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-	//	Handler:        router,
-	//	ReadTimeout:    setting.ReadTimeout,
-	//	WriteTimeout:   setting.WriteTimeout,
-	//	MaxHeaderBytes: 1 << 20,
+	//endless.DefaultReadTimeOut = setting.ReadTimeout
+	//endless.DefaultWriteTimeOut = setting.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPort := fmt.Sprintf(":%d", setting.HTTPPort)
+	//
+	//server := endless.NewServer(endPort, routers.InitRouter())
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
 	//}
-	//s.ListenAndServe()
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
+
+	router := routers.InitRouter()
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
