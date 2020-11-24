699daae1a6ef6887c243b8bbe6c9e8ea2932fdf7
liyang <liyangcoder@163.com>
2020-11-24 14:48:46 +0800

    windows对endless的支持不够,因此分为两个入口文件

--- main.go
+++ main.go
@@ -6,38 +6,21 @@ import (
 	"gin-blog/routers"
 	"github.com/fvbock/endless"
 	"log"
-	"net/http"
-	"runtime"
 	"syscall"
 )
 
 func main() {
-	goos := runtime.GOOS
-	switch goos {
-	case "linux":
-		endless.DefaultReadTimeOut = setting.ReadTimeout
-		endless.DefaultWriteTimeOut = setting.WriteTimeout
-		endless.DefaultMaxHeaderBytes = 1 << 20
-		endPort := fmt.Sprintf(":%d", setting.HTTPPort)
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPort := fmt.Sprintf(":%d", setting.HTTPPort)
 
-		server := endless.NewServer(endPort, routers.InitRouter())
-		server.BeforeBegin = func(add string) {
-			log.Printf("Actual pid is %d", syscall.Getpid())
-		}
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Printf("Server err: %v", err)
-		}
-	case "windows":
-		router := routers.InitRouter()
-		s := &http.Server{
-			Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-			Handler:        router,
-			ReadTimeout:    setting.ReadTimeout,
-			WriteTimeout:   setting.WriteTimeout,
-			MaxHeaderBytes: 1 << 20,
-		}
-		s.ListenAndServe()
+	server := endless.NewServer(endPort, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
-
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
